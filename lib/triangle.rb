761f02e24c2aee2311b88fd17f5b2dfda9fe8e3d
chad <chad.palmer@yale.edu>
2020-06-02 13:21:34 -0400

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,41 @@
 class Triangle
-  # write code here
+  attr_reader :sides
+
+  def initialize(a, b, c)
+    @sides = [a, b, c]
+  end
+
+  def negative_error
+    sides.any? {|side| not side.positive?}
+  end
+
+  def length_error
+    sides.any? {|side| 2*side >= sides.sum}
+  end
+  
+  def invalid
+    negative_error || length_error
+  end
+
+  def type_determiner
+    case sides.uniq.count
+    when 3
+      :scalene
+    when 2
+      :isosceles
+    when 1
+      :equilateral
+    end
+  end
+
+  def kind
+    if invalid
+      raise TriangleError
+    else
+      type_determiner
+    end
+  end
+
+  class TriangleError < StandardError
+  end
 end
